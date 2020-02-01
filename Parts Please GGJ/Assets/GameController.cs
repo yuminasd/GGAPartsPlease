e8b608979f96ca40b202288b0e1ffc09d4b8aaa4
Brett Miller <38405413+LLimb@users.noreply.github.com>
2020-01-31 23:31:48 -0700

    Scene Loader added and quit button functioning

---
+++ Parts Please GGJ/Assets/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update    
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!Input.GetKeyDown(KeyCode.Mouse0) && !Input.GetKeyDown(KeyCode.Mouse1))
+        if (Input.anyKeyDown)
+        {
+            SceneLoader.GoToScene(SceneLoader.Scene.MainGame);
+        }    
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("I QUIT THE GAME");
+        Application.Quit();
+    }
+
+}
