835893f5dc4bae30b476de7770a31232f4853e4c
Amer <ajami.amer1@gmail.com>
2020-02-02 06:03:38 -0700

    background cycling

--- Parts Please GGJ/Assets/GameTimer.cs
+++ Parts Please GGJ/Assets/GameTimer.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using PartsPlease;
 
 public class GameTimer : MonoBehaviour
@@ -8,7 +9,7 @@ public class GameTimer : MonoBehaviour
     [SerializeField]
     private int CurrentTime;
     private int StartTime = 0;
-    private int EndTime = 180;
+    private int EndTime = 30;
     [SerializeField]
     private float Timer;
     private bool KeepTime;
@@ -17,21 +18,47 @@ public class GameTimer : MonoBehaviour
     public GameObject OpenShopButton;
     public Animator Anim_ShipDoor;
 
+    public SpriteRenderer sr;
+    public Sprite[] sprites = new Sprite[6];
+    private int imageIndex;
+    private float switchTime;
+    private float switchTimer;
+
+
+   
+
+
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("starting timer");
+
         this.KeepTime = false;
         this.CurrentTime = 0;
         this.Timer = 0;
+    
+        this.imageIndex = 0;
+        this.switchTimer = 0;
+        this.switchTime = EndTime / 6;
+
+        sr = GetComponentInChildren<SpriteRenderer>();
+
+        sr.sprite = sprites[0];
+
+      
     }
 
     // Update is called once per frame
     void Update()
     {
+        switchBackGroundImages();
+
         if (this.KeepTime)
         {
             this.Timer += Time.deltaTime;
+            this.switchTimer += Time.deltaTime;
             this.CurrentTime = Mathf.FloorToInt(this.Timer);
+            Debug.Log("time is going" + switchTimer + "is compared to: " + switchTime);
         }
         if(this.Timer >= this.EndTime)
         {
@@ -50,6 +77,7 @@ public class GameTimer : MonoBehaviour
     {
         this.CurrentTime = 0;
         this.Timer = 0;
+        this.switchTimer = 0;
     }
 
     public void StartTimer()
@@ -77,5 +105,28 @@ public class GameTimer : MonoBehaviour
     {
         Anim_ShipDoor.SetTrigger("Trig_CloseShop");
     }
+    public void closeImages ()
+    {
+        sr.sprite = sprites[imageIndex];
+
+    }
+    private void switchBackGroundImages ()
+    {
+       
+        if (switchTimer >= switchTime)
+        {
+            Debug.Log("Time switch");
+            switchTimer = 0;
+            imageIndex++;
+            closeImages();
+            
+            if (imageIndex >= 6)
+            {
+                imageIndex = 0;
+            }
+        }
+
+    }
+
 
 }
