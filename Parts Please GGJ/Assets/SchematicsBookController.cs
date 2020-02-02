252dd9de759cca96681e8845e760fbdbe182dd4f
Jerry Rice <grundyboy34@gmail.com>
2020-02-01 19:34:35 -0700

    Schematics Book basics complete

---
+++ Parts Please GGJ/Assets/SchematicsBookController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PartsPlease;
+
+namespace PartsPlease
+{
+    public enum Page
+    {
+        Cover,
+        Models,
+        Lights,
+        Scopes,
+        Gears
+    }
+
+    public class SchematicsBookController : MonoBehaviour
+    {
+
+        public GameObject schematicsBookOpen;
+        public GameObject schematicsBook;
+        public GameObject modelsPage, lightsPage, gearsPage, scopesPage;
+        Page page;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            page = Page.Models;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void onChildClick(Page desiredPage)
+        {
+            Debug.Log("desiring page" + desiredPage);
+            schematicsBookOpen.SetActive(true);
+            modelsPage.SetActive(false);
+            lightsPage.SetActive(false);
+            gearsPage.SetActive(false);
+            scopesPage.SetActive(false);
+            switch (desiredPage)
+            {
+                
+                case Page.Cover:
+                    schematicsBookOpen.SetActive(false);
+                    break;
+                case Page.Models:
+                    modelsPage.SetActive(true);
+                    break;
+                case Page.Lights:
+                    lightsPage.SetActive(true);
+                    break;
+                case Page.Gears:
+                    gearsPage.SetActive(true);
+                    break;
+                case Page.Scopes:
+                    scopesPage.SetActive(true);
+                    break;
+            }
+        }
+    }
+}
---
+++ Parts Please GGJ/Assets/Scripts/Armory.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PartsPlease
+{
+    public class Armory
+    {
+        private static Armory INSTANCE;
+        public List<Item> itemModels = new List<Item>();
+
+        private Armory()
+        {
+            //add items to armory here (these are what are used to display in schematics, and to check if repair was correct)
+
+        }
+
+        public static Armory get()
+        {
+            if (INSTANCE == null)
+            {
+                INSTANCE = new Armory();
+            }
+            return INSTANCE;
+        }
+    }
+
+}
