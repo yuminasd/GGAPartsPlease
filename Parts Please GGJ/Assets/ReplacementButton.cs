ef3da08cd7f07a3b2c7bc2ef1e154021fbcf9d9f
Brett Miller <38405413+LLimb@users.noreply.github.com>
2020-02-02 14:06:01 -0700

    weapon replacer in game

---
+++ Parts Please GGJ/Assets/ReplacementButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PartsPlease;
+using UnityEngine.UI;
+
+public class ReplacementButton : MonoBehaviour
+{
+    public PartType replacementType;
+    public ReplacementChooserController parent;
+    public Button activationButton;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Start()
+    {
+        activationButton.onClick.AddListener(delegate { parent.onReplacementClick(replacementType); });
+    }
+}
