7659d94a54575759ecded8512d23940d183295f9
Amer <ajami.amer1@gmail.com>
2020-02-01 19:09:55 -0700

    amer folder

---
+++ Parts Please GGJ/Assets/amerFolder/Scripts/Part.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PartsPlease
+{
+    public enum PartState
+    {
+        Working,
+        NotWorking
+    }
+
+
+    public enum PartType
+    {
+        LightBulb,
+        Pipe,
+        Hose,
+    }
+
+
+
+
+
+    public class Part : MonoBehaviour
+    {
+
+        public RawImage partImage;
+        public RawImage[] partImages;
+        public int x;
+        public int y;
+
+
+        short serialNumber;
+        public PartState state;
+
+
+        void Start()
+        {
+
+     
+        }
+
+    
+
+        public void assignAttributes ()
+        {
+            
+            int i = Random.Range(0, 3);
+            Debug.Log("starting new item:" + i);
+ 
+
+            switch (i) {
+                case 0:
+                    partImage.texture = partImages[0].texture;
+                    break;
+                case 1:
+                    partImage.texture = partImages[1].texture;
+                    break;
+                case 2:
+                    partImage.texture = partImages[2].texture;
+                    break;
+
+
+            }
+
+            partImage.rectTransform.sizeDelta = new Vector2(50, 50);
+
+        }
+
+
+
+    }   
+
+}
---
+++ Parts Please GGJ/Assets/amerFolder/Scripts/animations.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animations : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Animator animator;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void openDialogueBox ()
+    {
+        animator.SetBool("isOpen", true);
+
+
+
+
+    }
+}
